700bf4b4976f6011017cca845c3ea688fb491f6f
Enrico Foschi <foschi.enrico@gmail.com>
2015-05-23 20:03:03 +0200

    Basic blocker integration

--- client/template/mytemplate.js
+++ client/template/mytemplate.js
@@ -5,8 +5,10 @@ Template.MyTemplate.events({
 	'click .btn-primary': function() {
 
 		/* Calling the server side pay method */
+		$('.blocker').stop(true, false).fadeIn(500);
 		Meteor.call('pay', function (errors, result) {
 			if(result) {
+				$('.blocker').stop(true, false).fadeOut(250);
 				alert("Paid");
 				// Here you move on to the next page.
 			}
